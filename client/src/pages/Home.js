115eb0e9aa0d973402ad6dfb83c4a28ddadc62f6
BillStephens2022 <two4onebill@yahoo.com>
2023-05-23 16:31:43 -0400

    added photo to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,9 @@ const Home = () => {
     <div className="main">
         <h1 className="title">BarKEEP</h1>
         <h2 className="subtitle">Access your favorite cocktail recipes here!</h2>
+        <div className='home_photo'>
+          <img className='home_photo_image' src='https://www.tastingtable.com/img/gallery/11-cocktails-to-try-if-you-like-drinking-gin/intro-1659025591.webp'></img>
+        </div>
     </div>
   );
    
