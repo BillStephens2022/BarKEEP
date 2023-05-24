ed8357e95c7279407ade5cb994f868564d10eeb1
BillStephens2022 <two4onebill@yahoo.com>
2023-05-24 11:07:54 -0400

    added basic login form html elements

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Button from '../components/Button';
 
 const LoginForm = () => {
     return (
         <div className='login_form_page'>
           <h3>Login Form</h3>
           <form className='login_form'>
-
+            <label className="form_label" htmlFor='eamil'>Email</label>
+            <input className="form_input"></input>
+            <label className="form_label" htmlFor='password'>Password</label>
+            <input className="form_input"></input>
+            <Button text='Log In!'></Button>
           </form>
         </div>
     )
