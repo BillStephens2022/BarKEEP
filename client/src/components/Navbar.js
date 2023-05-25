6e1a0033e3103967c35da3d055b26a62bf6345d3
BillStephens2022 <two4onebill@yahoo.com>
2023-05-25 16:38:28 -0400

    updated Navbar to show different items depending on whether user is logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
-
 const Navbar = () => {
-
   return (
     <div className="navigation">
-      <ul className='navigation_list'>
-        <li className='navigation_item'>
-          <Link to='/' className='navigation_link'>Home</Link>
+      <ul className="navigation_list">
+        <li className="navigation_item">
+          <Link to="/" className="navigation_link">
+            Home
+          </Link>
         </li>
-        <li className='navigation_item'>
-          <Link to='/cocktails' className='navigation_link'>My Cocktails</Link>
-        </li>
-        <li className='navigation_item'>
-          <Link to='/login' className='navigation_link'>Log In / Register</Link>
-        </li>
-        <li className='navigation_item'>
-          <Link to='/logout' className='navigation_link' onClick={Auth.logout}>Log Out</Link>
-        </li>
+        {Auth.loggedIn() ? (
+          <>
+            <li className="navigation_item">
+              <Link to="/cocktails" className="navigation_link">
+                My Cocktails
+              </Link>
+            </li>
+            <li className="navigation_item">
+              <Link
+                to="/logout"
+                className="navigation_link"
+                onClick={Auth.logout}
+              >
+                Log Out
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="navigation_item">
+            <Link to="/login" className="navigation_link">
+              Log In / Register
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
-  )
-   
+  );
 };
 
 export default Navbar;
