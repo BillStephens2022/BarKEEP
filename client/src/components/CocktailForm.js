f67ce5bf67f48e4c3c9565f143adf17107ccbbb6
BillStephens2022 <two4onebill@yahoo.com>
2023-05-27 12:36:41 -0400

    added initial styling to cocktail form

--- client/src/components/CocktailForm.js
+++ client/src/components/CocktailForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/Home.css";
+import "../styles/CocktailForm.css";
 import { Button } from "react-bootstrap";
 
 const CocktailForm = () => {
