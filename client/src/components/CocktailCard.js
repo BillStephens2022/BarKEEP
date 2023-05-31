363e08b446ecedcd09e210e2ee37653eeedb87ec
BillStephens2022 <two4onebill@yahoo.com>
2023-05-31 15:52:24 -0400

    updated CocktailCard component to have a front and back and added some CSS styling

--- client/src/components/CocktailCard.js
+++ client/src/components/CocktailCard.js
@@ -1,9 +1,17 @@
-import React from "react";
-import "../styles/Home.css";
+import React, { useState } from "react";
+import "../styles/CocktailCard.css";
 import { GoPencil, GoTrashcan } from "react-icons/go";
 import Auth from "../utils/auth";
 
-const CocktailCard = ({ data, loading, cocktails, setCocktails, deleteCocktail }) => {
+const CocktailCard = ({
+  data,
+  loading,
+  cocktails,
+  setCocktails,
+  deleteCocktail,
+}) => {
+  const [flippedCard, setFlippedCard] = useState(null);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -17,7 +25,7 @@ const CocktailCard = ({ data, loading, cocktails, setCocktails, deleteCocktail }
     const cocktailId = e.currentTarget.id;
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     if (!token) return false;
-    console.log("deleting cocktail!")
+    console.log("deleting cocktail!");
     try {
       const { data } = await deleteCocktail({
         variables: { cocktailId },
@@ -29,53 +37,65 @@ const CocktailCard = ({ data, loading, cocktails, setCocktails, deleteCocktail }
     } catch (err) {
       console.error(err);
     }
-    setCocktails(
-      cocktails.filter((cocktails) => cocktails._id !== cocktailId)
-    );
+    setCocktails(cocktails.filter((cocktails) => cocktails._id !== cocktailId));
     console.log(data);
-  }
+  };
 
   const handleEditCocktail = () => {
-    console.log("editing cocktail!")
+    console.log("editing cocktail!");
     return;
-  }
+  };
+
+  const handleCardFlip = (cocktailId) => {
+    setFlippedCard(cocktailId);
+  };
 
   return (
     <>
       {cocktails.map((cocktail) => (
-        <div className="card" key={cocktail._id}>
-          <div className="card_title">{cocktail.name}</div>
-          <div className="card_body">
-            <div className="card_photo">
-              <img
-                className="cocktail_photo"
-                src={cocktail.imageURL}
-                alt="cocktail"
-              ></img>
+        <div
+          className={`card ${flippedCard === cocktail._id ? "flipped" : ""}`}
+          key={cocktail._id}
+          onClick={() => handleCardFlip(cocktail._id)}
+        >
+          <div className="card-side card-front">
+            <div className="card_title">{cocktail.name}</div>
+            <div className="card_body">
+              <div className="card_photo">
+                <img
+                  className="cocktail_photo"
+                  src={cocktail.imageURL}
+                  alt="cocktail"
+                ></img>
+              </div>
+              <div className="card_recipe">
+                <ul className="ingredients_list">
+                  {cocktail.ingredients.map((ingredient, index) => (
+                    <li className="ingredient" key={index}>
+                      {ingredient.quantity} {ingredient.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <div className="card_recipe">
-              <ul className="ingredients_list">
-                {cocktail.ingredients.map((ingredient, index) => (
-                <li className="ingredient" key={index}>{ingredient.quantity} {ingredient.name}</li>
-                ))}
-              </ul>
-              <h5>Instructions</h5>
-              <p className="p_instructions">{cocktail.instructions}</p>
-            </div>
+          </div>
+          <div className="card-side card-back">
+            <h5>Instructions</h5>
+            <p className="p_instructions">{cocktail.instructions}</p>
           </div>
           <div className="card_footer">
             <button
               className="btn"
               id={cocktail._id}
               onClick={handleDeleteCocktail}
-              >
+            >
               <GoTrashcan />
             </button>
             <button
               className="btn"
               id={cocktail._id}
               onClick={handleEditCocktail}
-              >
+            >
               <GoPencil />
             </button>
           </div>
