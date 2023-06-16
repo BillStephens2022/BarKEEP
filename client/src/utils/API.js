a29663c0af6cf00156b12f5ab29f9b21b6256789
BillStephens2022 <two4onebill@yahoo.com>
2023-06-16 16:13:56 -0400

    updated API endpoint to be https instead of http to fix error in API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,15 @@
 // make a search to 'TheCocktailDB' api
 // url to search for a cocktail by name: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 export const searchCocktails = async (query) => {
-    console.log(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
-    try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
-        return response.json();
-    } catch (err) {
-        console.error('Error fetching cocktail data: ', err);
-        throw err;
+  console.log(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+  try {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
     }
+    return response.json();
+  } catch (err) {
+    console.error('Error fetching cocktail data: ', err);
+    throw err;
+  }
 };
