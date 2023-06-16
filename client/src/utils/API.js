3502dcf1729dc685b774570754ac38d84431a612
BillStephens2022 <two4onebill@yahoo.com>
2023-06-16 16:02:09 -0400

    updated searchCocktails util function to be asynchronous since it is an API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,12 @@
 // make a search to 'TheCocktailDB' api
 // url to search for a cocktail by name: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-export const searchCocktails = (query) => {
-    return fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+export const searchCocktails = async (query) => {
+    console.log(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+    try {
+        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
+        return response.json();
+    } catch (err) {
+        console.error('Error fetching cocktail data: ', err);
+        throw err;
+    }
 };
