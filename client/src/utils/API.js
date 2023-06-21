961d6abe645258b87ea024de4041b4b24be580ce
BillStephens2022 <two4onebill@yahoo.com>
2023-06-21 12:07:26 -0400

    minor change, removed console.log

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,6 @@
 // make a search to 'TheCocktailDB' api
 // url to search for a cocktail by name: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 export const searchCocktails = async (query) => {
-  console.log(
-    `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
-  );
   try {
     const response = await fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
@@ -42,9 +39,6 @@ export const getCocktailsbyIngredient = async (ingredient) => {
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
     }
-    // const searchResults = await response.json();
-    // console.log(searchResults.drinks[0]);
-
     return response;
   } catch (err) {
     console.error("Error fetching cocktail data: ", err);
