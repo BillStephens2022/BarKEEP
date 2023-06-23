4a73039f10aa5f47986b4dafffaa2a5dd87eccde
BillStephens2022 <two4onebill@yahoo.com>
2023-06-23 14:03:49 -0400

    refactored API call utility functions as there was duplicated code for formatting data received in the response

--- client/src/pages/RandomCocktail.js
+++ client/src/pages/RandomCocktail.js
@@ -6,44 +6,13 @@ const RandomCocktail = () => {
   const [randomCocktail, setRandomCocktails] = useState([]);
 
   useEffect(() => {
-  const fetchRandomCocktail = async () => {
-    try {
-      const response = await getRandomCocktail();
-      const randomCocktail = await response.json();
-      console.log(randomCocktail.drinks[0].strDrink);
-      const cocktailData = randomCocktail.drinks.map((cocktail) => {
-        const ingredients = [];
-        const tags = [];
-        for (let i = 1; i <= 15; i++) {
-          const ingredient = cocktail[`strIngredient${i}`];
-          const quantity = cocktail[`strMeasure${i}`];
-          if (ingredient) {
-            ingredients.push({name: ingredient, quantity: quantity});
-            tags.push(ingredient)
-          } else {
-            break; // stop iterating if no more ingredients are found
-          }
-        }
-        return {
-        _id: cocktail.idDrink,
-        name: cocktail.strDrink,
-        imageURL: cocktail.strDrinkThumb,
-        instructions: cocktail.strInstructions,
-        glassware: cocktail.strGlass,
-        ingredients: ingredients,
-        tags: tags,
-        };
-      });
-      // will need to map ingredients and quantities from API call into ingredients array.
-      console.log("SEARCH RESULTS: ", cocktailData);
+    const fetchRandomCocktail = async () => {
+      const cocktailData = await getRandomCocktail();
       setRandomCocktails(cocktailData);
-    } catch (error) {
-      console.error("Error fetching cocktail data:", error);
-    }
-  };
-  fetchRandomCocktail();
-}, []);
-  
+    };
+    fetchRandomCocktail();
+  }, []);
+
   return (
     <div>
       <h1 className="title">BarKEEP</h1>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,40 +1,42 @@
-// make a search to 'TheCocktailDB' api
-// url to search for a cocktail by name: www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+const formatCocktailData = (cocktail) => {
+  const ingredients = [];
+  const tags = [];
+
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = cocktail[`strIngredient${i}`];
+    const quantity = cocktail[`strMeasure${i}`];
+
+    if (ingredient) {
+      ingredients.push({ name: ingredient, quantity: quantity });
+      tags.push(ingredient);
+    } else {
+      break;
+    }
+  }
+
+  return {
+    _id: cocktail.idDrink,
+    name: cocktail.strDrink,
+    imageURL: cocktail.strDrinkThumb,
+    instructions: cocktail.strInstructions,
+    glassware: cocktail.strGlass,
+    ingredients: ingredients,
+    tags: tags,
+  };
+};
+
 export const searchCocktails = async (query) => {
   try {
     const response = await fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
     );
+
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
     }
+
     const searchResults = await response.json();
-    let cocktailData = [];
-    cocktailData = searchResults.drinks.map((cocktail) => {
-      const ingredients = [];
-      const tags = [];
-      // maps ingredients and quantities from API call into the ingredients array to be
-      // consistent with database schema.
-      for (let i = 1; i <= 15; i++) {
-        const ingredient = cocktail[`strIngredient${i}`];
-        const quantity = cocktail[`strMeasure${i}`];
-        if (ingredient) {
-          ingredients.push({ name: ingredient, quantity: quantity });
-          tags.push(ingredient);
-        } else {
-          break; // stop iterating if no more ingredients are found
-        }
-      }
-      return {
-        _id: cocktail.idDrink,
-        name: cocktail.strDrink,
-        imageURL: cocktail.strDrinkThumb,
-        instructions: cocktail.strInstructions,
-        glassware: cocktail.strGlass,
-        ingredients: ingredients,
-        tags: tags,
-      };
-    });
+    const cocktailData = searchResults.drinks.map(formatCocktailData);
     return cocktailData;
   } catch (err) {
     console.error("Error fetching cocktail data: ", err);
@@ -47,10 +49,14 @@ export const getRandomCocktail = async () => {
     const response = await fetch(
       "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     );
+
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
     }
-    return response;
+
+    const randomCocktail = await response.json();
+    const cocktailData = randomCocktail.drinks.map(formatCocktailData);
+    return cocktailData;
   } catch (err) {
     console.error("Error fetching cocktail data: ", err);
     throw err;
@@ -63,36 +69,13 @@ export const getCocktailsbyIngredient = async (ingredient) => {
     const response = await fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
     );
+
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
     }
+
     const searchResults = await response.json();
-    let cocktailData = [];
-    cocktailData = searchResults.drinks.map((cocktail) => {
-      const ingredients = [];
-      const tags = [];
-      // maps ingredients and quantities from API call into the ingredients array to be
-      // consistent with database schema.
-      for (let i = 1; i <= 15; i++) {
-        const ingredient = cocktail[`strIngredient${i}`];
-        const quantity = cocktail[`strMeasure${i}`];
-        if (ingredient) {
-          ingredients.push({ name: ingredient, quantity: quantity });
-          tags.push(ingredient);
-        } else {
-          break; // stop iterating if no more ingredients are found
-        }
-      }
-      return {
-        _id: cocktail.idDrink,
-        name: cocktail.strDrink,
-        imageURL: cocktail.strDrinkThumb,
-        instructions: cocktail.strInstructions,
-        glassware: cocktail.strGlass,
-        ingredients: ingredients,
-        tags: tags,
-      };
-    });
+    const cocktailData = searchResults.drinks.map(formatCocktailData);
     return cocktailData;
   } catch (err) {
     console.error("Error fetching cocktail data: ", err);
