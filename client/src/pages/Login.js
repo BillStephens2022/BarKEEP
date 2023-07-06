c855c0aaf66b2b516ea87817d01984fd6e42378a
BillStephens2022 <two4onebill@yahoo.com>
2023-07-06 15:08:41 -0400

    added new feature for potential users to log in as a guest

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,10 +3,14 @@ import "../styles/Home.css";
 import Button from "../components/Button";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = () => {
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const [showLoginForm, setShowLoginForm] = useState(false);
+  const [login] = useMutation(LOGIN_USER);
 
   const handleRegisterClick = () => {
     setShowRegisterForm(true);
@@ -18,12 +22,31 @@ const Login = () => {
     setShowLoginForm(true)
   };
 
+  const handleGuestClick = async () => {
+    console.log("logging in as guest!");
+    
+    const guestLoginData =  {
+      email: "guest@gmail.com",
+      password: "password"
+    }
+
+    try {
+      const { data } = await login( 
+        {variables : { ...guestLoginData }, 
+      });
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="login_page">
       <h1 className="title">BarKEEP</h1>
       <h2 className="subtitle">Log In / Register</h2>
       <Button onClick={handleLoginClick} text="Log In" />
       <Button onClick={handleRegisterClick} text="Register" />
+      <Button onClick={handleGuestClick} text="Guest Login" />
       <div className="form_container">
         {showRegisterForm ? (<RegisterForm />) : null}
         {showLoginForm ? (<LoginForm />): null}
