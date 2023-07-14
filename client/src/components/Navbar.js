00feeb3f8497d410e8141bbdc455fc319ba47528
BillStephens2022 <two4onebill@yahoo.com>
2023-07-14 15:03:19 -0400

    updated Navbar component with some highlight animation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Auth } from "../utils/auth";
 
 const Navbar = () => {
+  // state to track active Nav item for styling
+  const [activeNavItem, setActiveNavItem] = useState("");
+
+  // Update the active navigation item when clicked
+  const handleNavItemClick = (navItem) => {
+    setActiveNavItem(navItem);
+  };
+
   return (
     <div className="navigation">
       <ul className="navigation_list">
-        <li className="navigation_item">
+        <li
+          className={`navigation_item ${
+            activeNavItem === "home" ? "navigation_item--active" : ""
+          }`}
+          onClick={() => handleNavItemClick("home")}
+        >
           <Link to="/" className="navigation_link">
             Home
           </Link>
         </li>
         {Auth.loggedIn() ? (
           <>
-          <li className="navigation_item">
+            <li
+              className={`navigation_item ${
+                activeNavItem === "feed" ? "navigation_item--active" : ""
+              }`}
+              onClick={() => handleNavItemClick("feed")}
+            >
               <Link to="/feed" className="navigation_link">
                 Feed
               </Link>
             </li>
-            <li className="navigation_item">
+            <li
+              className={`navigation_item ${
+                activeNavItem === "favorites" ? "navigation_item--active" : ""
+              }`}
+              onClick={() => handleNavItemClick("favorites")}
+            >
               <Link to="/favorites" className="navigation_link">
                 Favorites
               </Link>
             </li>
-            <li className="navigation_item">
+            <li
+              className={`navigation_item ${
+                activeNavItem === "search" ? "navigation_item--active" : ""
+              }`}
+              onClick={() => handleNavItemClick("search")}
+            >
               <Link to="/searchCocktails" className="navigation_link">
                 Search
               </Link>
             </li>
-            
+
             <li className="navigation_item">
               <Link
                 to="/logout"
