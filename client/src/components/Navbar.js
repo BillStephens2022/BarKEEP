ee7b23b740c6d16621d1cccbde454cb55339597c
BillStephens2022 <two4onebill@yahoo.com>
2023-07-14 17:34:50 -0400

    updated Nav component to be hamburger menu on smaller screens.  still working on bug with menu button and how it is displayed on expansion and collapse

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,78 +3,169 @@ import { Link } from "react-router-dom";
 import { Auth } from "../utils/auth";
 
 const Navbar = () => {
-  // state to track active Nav item for styling
-  const [activeNavItem, setActiveNavItem] = useState("");
+  const [isExpanded, setIsExpanded] = useState(false); // State to track whether Navbar is expanded or not
+  const [activeNavItem, setActiveNavItem] = useState(""); // State to track active Nav item for styling
+
+  // To toggle between expanded/collapsed state
+  const toggleMenu = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   // Update the active navigation item when clicked
   const handleNavItemClick = (navItem) => {
     setActiveNavItem(navItem);
+    setIsExpanded(false);
   };
 
   return (
-    <div className="navigation">
-      <ul className="navigation_list">
-        <li
-          className={`navigation_item ${
-            activeNavItem === "home" ? "navigation_item--active" : ""
-          }`}
-          onClick={() => handleNavItemClick("home")}
-        >
-          <Link to="/" className="navigation_link">
-            Home
-          </Link>
-        </li>
-        {Auth.loggedIn() ? (
-          <>
-            <li
-              className={`navigation_item ${
-                activeNavItem === "feed" ? "navigation_item--active" : ""
-              }`}
-              onClick={() => handleNavItemClick("feed")}
-            >
-              <Link to="/feed" className="navigation_link">
-                Feed
-              </Link>
-            </li>
-            <li
-              className={`navigation_item ${
-                activeNavItem === "favorites" ? "navigation_item--active" : ""
-              }`}
-              onClick={() => handleNavItemClick("favorites")}
-            >
-              <Link to="/favorites" className="navigation_link">
-                Favorites
-              </Link>
-            </li>
-            <li
-              className={`navigation_item ${
-                activeNavItem === "search" ? "navigation_item--active" : ""
-              }`}
-              onClick={() => handleNavItemClick("search")}
-            >
-              <Link to="/searchCocktails" className="navigation_link">
-                Search
-              </Link>
-            </li>
-
-            <li className="navigation_item">
-              <Link
-                to="/logout"
-                className="navigation_link"
-                onClick={Auth.logout}
-              >
-                Log Out
-              </Link>
-            </li>
-          </>
-        ) : (
-          <li className="navigation_item">
-            <Link to="/login" className="navigation_link">
-              Log In / Register
+    <div className={`navigation ${isExpanded ? "expanded" : ""}`}>
+      {/* Sidebar */}
+      <nav className="sidebar">
+        <ul className="navigation_list">
+          <li
+            className={`navigation_item ${
+              activeNavItem === "home" ? "navigation_item--active" : ""
+            }`}
+            onClick={() => handleNavItemClick("home")}
+          >
+            <Link to="/" className="navigation_link">
+              Home
             </Link>
           </li>
-        )}
-      </ul>
+          {Auth.loggedIn() ? (
+            <>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "feed" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("feed")}
+              >
+                <Link to="/feed" className="navigation_link">
+                  Feed
+                </Link>
+              </li>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "favorites" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("favorites")}
+              >
+                <Link to="/favorites" className="navigation_link">
+                  Favorites
+                </Link>
+              </li>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "search" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("search")}
+              >
+                <Link to="/searchCocktails" className="navigation_link">
+                  Search
+                </Link>
+              </li>
+
+              <li className="navigation_item">
+                <Link
+                  to="/logout"
+                  className="navigation_link"
+                  onClick={() => {
+                    handleNavItemClick("");
+                    Auth.logout();
+                  }}
+                >
+                  Log Out
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li className="navigation_item">
+              <Link to="/login" className="navigation_link">
+                Log In / Register
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+
+      {/* Hamburger Menu */}
+      <div className={`hamburger-menu ${isExpanded ? "expanded" : ""}`}>
+        <button
+          className={`menu-toggle ${isExpanded ? "expanded" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className={`hamburger ${isExpanded ? "expanded" : ""}`}>
+            <span className="hamburger-1"></span>
+            <span className="hamburger-2"></span>
+            <span className="hamburger-3"></span>
+          </span>
+        </button>
+        <ul className={`navigation_list ${isExpanded ? "expanded" : ""}`}>
+          <li
+            className={`navigation_item ${
+              activeNavItem === "home" ? "navigation_item--active" : ""
+            }`}
+            onClick={() => handleNavItemClick("home")}
+          >
+            <Link to="/" className="navigation_link">
+              Home
+            </Link>
+          </li>
+          {Auth.loggedIn() ? (
+            <>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "feed" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("feed")}
+              >
+                <Link to="/feed" className="navigation_link">
+                  Feed
+                </Link>
+              </li>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "favorites" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("favorites")}
+              >
+                <Link to="/favorites" className="navigation_link">
+                  Favorites
+                </Link>
+              </li>
+              <li
+                className={`navigation_item ${
+                  activeNavItem === "search" ? "navigation_item--active" : ""
+                }`}
+                onClick={() => handleNavItemClick("search")}
+              >
+                <Link to="/searchCocktails" className="navigation_link">
+                  Search
+                </Link>
+              </li>
+
+              <li className="navigation_item">
+                <Link
+                  to="/logout"
+                  className="navigation_link"
+                  onClick={() => {
+                    handleNavItemClick("");
+                    Auth.logout();
+                  }}
+                >
+                  Log Out
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li className="navigation_item">
+              <Link to="/login" className="navigation_link">
+                Log In / Register
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
