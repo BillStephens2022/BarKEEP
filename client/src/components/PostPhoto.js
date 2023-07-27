5537fa40340872c1efe56c208e956e0fc08c559f
BillStephens2022 <two4onebill@yahoo.com>
2023-07-27 15:27:46 -0400

    added modals so when post photos get clicked on they expand into a modal

--- client/src/components/PostPhoto.js
+++ client/src/components/PostPhoto.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import cloudinary from "cloudinary-core";
+import { Modal } from "react-bootstrap";
+import "../styles/PostPhoto.css";
 
-const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME });
+const cloudinaryCore = new cloudinary.Cloudinary({
+  cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+});
 
 const PostPhoto = ({ imageUrl }) => {
-    const transformedImageUrl = cloudinaryCore.url(imageUrl, {
-      width: 500,
-      height: 500,
-      crop: "fill_pad",
-    });
-  
-    return (
-      <img className={"post-photo"} src={transformedImageUrl} alt="post" />
-    );
+  const transformedImageUrl = cloudinaryCore.url(imageUrl, {
+    width: 500,
+    height: 500,
+    crop: "fill_pad",
+  });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleToggleModal = () => {
+    setIsModalOpen((prev) => !prev);
   };
+
+  return (
+    <>
+      <img
+        className="post-photo"
+        src={transformedImageUrl}
+        alt="post"
+        onClick={handleToggleModal}
+      />
+
+      <Modal show={isModalOpen} onHide={handleToggleModal} centered>
+        <Modal.Body>
+          <img
+            className="modal-photo"
+            src={transformedImageUrl}
+            alt="post"
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
 
 export default PostPhoto;
