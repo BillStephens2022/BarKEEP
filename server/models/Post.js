b5b1d2a6c209fc8b047af195004245c46a0ddb34
BillStephens2022 <two4onebill@yahoo.com>
2023-07-29 08:04:26 -0400

    updated Post model with commentSchema

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,16 @@
 const { Schema, model } = require("mongoose");
 
+const commentSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
 const postSchema = new Schema({
   postTitle: {
     type: String,
@@ -20,6 +31,13 @@ const postSchema = new Schema({
     ref: "User",
     required: true,
   },
+  likes: [
+    {
+      type: Number,
+      default: 0,
+    }
+  ],
+  comments: [commentSchema],
 });
 
 const Post = model("Post", postSchema);
