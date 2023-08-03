d667658fd36cec603740d7d1b542ecabe493b450
BillStephens2022 <two4onebill@yahoo.com>
2023-08-03 16:59:08 -0400

    updated styling for post like modal

--- client/src/components/PostLikesModal.js
+++ client/src/components/PostLikesModal.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { Modal, Button } from 'react-bootstrap'; 
-import { GET_POST_LIKES_USERS, GET_SINGLE_POST } from '../utils/queries'; 
-import ProfilePhoto from './ProfilePhoto';
-import SinglePost from './SinglePost';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { Modal, Button } from "react-bootstrap";
+import { GET_POST_LIKES_USERS, GET_SINGLE_POST } from "../utils/queries";
+import ProfilePhoto from "./ProfilePhoto";
+import SinglePost from "./SinglePost";
 import "../styles/PostLikesModal.css";
 
 const PostLikesModal = ({ postId, onClose }) => {
@@ -18,28 +18,25 @@ const PostLikesModal = ({ postId, onClose }) => {
 
   const post = postData?.getSinglePost;
 
-
   return (
-    
-    <Modal show={true} onHide={onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Liked by:</Modal.Title>
+    <Modal className="post-likes-modal" show={true} onHide={onClose}>
+      <Modal.Header className="post-likes-modal-header">
+        <Modal.Title className="post-likes-modal-title">Liked by:</Modal.Title>
+        <button
+          type="button"
+          className="post-likes-modal-btn-close"
+          aria-label="Close"
+          onClick={onClose}
+        >x</button>
       </Modal.Header>
       <Modal.Body>
         {loading || postLoading ? (
           <p>Loading...</p>
         ) : (
-            <div>
-            {post && (
-              
-                <SinglePost postId={postId} />
-                
-            
-            )}
-            <h3>Liked by:</h3>
-            <ul>
+          <div>
+            <ul className="post-likes-modal-ul">
               {data?.postLikesUsers?.map((like) => (
-                <li key={like._id}>
+                <li className="post-likes-modal-li" key={like._id}>
                   <ProfilePhoto
                     imageUrl={
                       like.profilePhoto ||
@@ -51,10 +48,12 @@ const PostLikesModal = ({ postId, onClose }) => {
                 </li>
               ))}
             </ul>
+            <h3 className="post-likes-modal-subtitle">Post:</h3>
+            {post && <SinglePost postId={postId} />}
           </div>
         )}
       </Modal.Body>
-      <Modal.Footer>
+      <Modal.Footer className="post-likes-modal-footer">
         <Button onClick={onClose}>Close</Button>
       </Modal.Footer>
     </Modal>
