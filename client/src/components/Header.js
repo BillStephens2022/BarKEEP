cdaa557788be8940e78b084805350ab38ad9d7e0
BillStephens2022 <two4onebill@yahoo.com>
2023-08-06 16:31:03 -0400

    updated content to About page and styled

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/components/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header-title">BarKEEP</h1>
+      <h2 className="header-subtitle">About</h2>
+    </div>
+  );
+};
+
+export default Header;
