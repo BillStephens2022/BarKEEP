b88834f6edbe517e7dc4f6187073e53e5308fb5d
BillStephens2022 <two4onebill@yahoo.com>
2023-08-12 09:58:09 -0400

    added modal for profile pic when clicked

--- client/src/components/ProfilePhoto.js
+++ client/src/components/ProfilePhoto.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import cloudinary from "cloudinary-core";
+import { Modal } from "react-bootstrap";
 
 const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME });
 
 const ProfilePhoto = ({ imageUrl, size }) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleToggleModal = () => {
+      setIsModalOpen((prev) => !prev);
+    };
+
     const transformedImageUrl = cloudinaryCore.url(imageUrl, {
       width: size,
       height: size,
@@ -12,7 +20,19 @@ const ProfilePhoto = ({ imageUrl, size }) => {
     });
   
     return (
-      <img className={`profile-photo post-author-image size-${size}`} src={transformedImageUrl} alt="Profile" />
+      <>
+      <img className={`profile-photo post-author-image size-${size}`} src={transformedImageUrl} onClick={handleToggleModal} alt="Profile" />
+      <Modal
+        show={isModalOpen}
+        onHide={handleToggleModal}
+        centered
+        animation={true}
+      >
+        <Modal.Body>
+          <img className="modal-profile-photo" src={transformedImageUrl} alt="user profile" />
+        </Modal.Body>
+      </Modal>
+      </>
     );
   };
 
