7c89e403229e08953494ceac61005e2745276632
BillStephens2022 <two4onebill@yahoo.com>
2023-08-13 15:47:26 -0400

    updated shimmer loader with more lines and a header

--- client/src/components/ShimmerLoader.js
+++ client/src/components/ShimmerLoader.js
@@ -4,6 +4,13 @@ import "../styles/components/ShimmerLoader.css";
 const ShimmerLoader = () => {
   return (
     <div className="shimmer-loader gradient-background">
+    <h1 className="shimmer-loader-h1">Loading</h1>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
       <div className="shimmer-line"></div>
       <div className="shimmer-line"></div>
       <div className="shimmer-line"></div>
