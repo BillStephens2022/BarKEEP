8bbe5568839ae0fd6577fb78f24aca926dc47e32
BillStephens2022 <two4onebill@yahoo.com>
2023-08-17 08:43:38 -0400

    updated comments on Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,15 +6,17 @@ import MyFavorites from "../components/MyFavorites";
 import "../styles/pages/Profile.css";
 
 const Profile = () => {
-  // State to manage view - see My Posts or My Favorite Recipes
+  // State to manage the view - "myFavorites" or "myPosts"
   const [view, setView] = useState("myFavorites");
 
   return (
     <div className="profile">
+      {/* Display the header with "Profile" title */}
       <Header page="profile" subtitle="Profile" />
-
+      {/* Buttons to switch between "Favorite Recipes" and "My Posts" views */}
       <div className="profile-button-div">
         <h3>View:</h3>
+        {/* Button to view favorite recipes */}
         <button
           className={`btn btn-view btn-my-favorites ${
             view === "myFavorites" ? "active" : ""
@@ -23,6 +25,7 @@ const Profile = () => {
         >
           Favorite Recipes
         </button>
+        {/* Button to view user's posts */}
         <button
           className={`btn btn-view btn-my-posts ${
             view === "myPosts" ? "active" : ""
@@ -32,7 +35,7 @@ const Profile = () => {
           My Posts
         </button>
       </div>
-
+      {/* Conditional rendering based on the selected view */}
       {view === "myPosts" ? <MyPosts /> : <MyFavorites />}
     </div>
   );
