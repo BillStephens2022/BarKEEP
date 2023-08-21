6feb9dcfb700bb98d1c2547ab622f59e3832017c
BillStephens2022 <two4onebill@yahoo.com>
2023-08-21 15:03:20 -0400

    updated comments in RandomCocktail component

--- client/src/components/RandomCocktail.js
+++ client/src/components/RandomCocktail.js
@@ -3,8 +3,10 @@ import { getRandomCocktail } from "../utils/API";
 import CocktailCard from "./CocktailCard";
 
 const RandomCocktail = ({ handleAddCocktail, addedCocktailId, cocktailAdded }) => {
+  // State to store the random cocktail data
   const [randomCocktail, setRandomCocktails] = useState([]);
 
+  // Fetch a random cocktail data from API when component mounts
   useEffect(() => {
     const fetchRandomCocktail = async () => {
       const cocktailData = await getRandomCocktail();
@@ -15,6 +17,7 @@ const RandomCocktail = ({ handleAddCocktail, addedCocktailId, cocktailAdded }) =
 
   return (
     <div className="card-container">
+      {/* Render the CocktailCard component with the fetched random cocktail data */}
       <CocktailCard
         cocktails={randomCocktail}
         page="SearchCocktails"
