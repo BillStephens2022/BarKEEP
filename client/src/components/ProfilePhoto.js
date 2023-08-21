839cf5c14fc0848ce3374e9540fadafdd7020542
BillStephens2022 <two4onebill@yahoo.com>
2023-08-21 15:01:24 -0400

    updated comments in ProfilePhoto component

--- client/src/components/ProfilePhoto.js
+++ client/src/components/ProfilePhoto.js
@@ -2,26 +2,40 @@ import React, { useState } from "react";
 import cloudinary from "cloudinary-core";
 import { Modal } from "react-bootstrap";
 
-const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME });
+// Initialize Cloudinary
+const cloudinaryCore = new cloudinary.Cloudinary({
+  cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+});
 
 const ProfilePhoto = ({ imageUrl, size, customClass }) => {
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  // To manage the modal's open/close state
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleToggleModal = () => {
-      setIsModalOpen((prev) => !prev);
-    };
-
-    const transformedImageUrl = cloudinaryCore.url(imageUrl, {
-      width: size,
-      height: size,
-      crop: "fill",
-      radius: "max",
-    });
+  // Function to toggle the modal's state
+  const handleToggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
   
-    return (
-      <>
-      <img className={`profile-photo post-author-image size-${size} ${customClass}`} src={transformedImageUrl} onClick={handleToggleModal} alt="Profile" />
+  // Transform the image URL using Cloudinary
+  const transformedImageUrl = cloudinaryCore.url(imageUrl, {
+    width: size,
+    height: size,
+    crop: "fill",
+    radius: "max",
+  });
+
+  return (
+    <>
+      {/* Profile photo thumbnail */}
+      <img
+        className={`profile-photo post-author-image size-${size} ${customClass}`}
+        src={transformedImageUrl}
+        onClick={handleToggleModal}
+        alt="Profile"
+      />
+
+      {/* Modal for displaying the enlarged profile photo */}
       <Modal
         show={isModalOpen}
         onHide={handleToggleModal}
@@ -29,11 +43,15 @@ const ProfilePhoto = ({ imageUrl, size, customClass }) => {
         animation={true}
       >
         <Modal.Body>
-          <img className="modal-profile-photo" src={transformedImageUrl} alt="user profile" />
+          <img
+            className="modal-profile-photo"
+            src={transformedImageUrl}
+            alt="user profile"
+          />
         </Modal.Body>
       </Modal>
-      </>
-    );
-  };
+    </>
+  );
+};
 
 export default ProfilePhoto;
